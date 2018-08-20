312db2a271c089bd7f6b2d5cd164d30c0e0a0ea3
suncab2 <suncab2@email.com>
2018-08-20 16:53:35 +0530

    new mode contact.js introduced

---
+++ server/src/models/contact.js
@@ -0,0 +1,25 @@
+/* JSHINT ES6 VERSION */
+module.exports = (sequelize, DataTypes) => {
+  const Contact= sequelize.define('contact',
+	  {
+	  id: { type: DataTypes.INTEGER,
+		  primaryKey: true
+	  },
+    name: DataTypes.INTEGER,
+    addrStrt: DataTypes.STRING,
+    addrCty: DataTypes.STRING,
+    addrPin: DataTypes.INTEGER,
+    gstin: DataTypes.STRING,
+    ccode: DataTypes.STRING,
+    pan: DataTypes.STRING,
+    cperson: DataTypes.STRING,
+    tel: DataTypes.STRING,
+    email:DataTypes.STRING,
+
+    },
+    { freezeTableName: true,
+      timestamps: false
+    }
+  )
+  return Contact ;
+}
