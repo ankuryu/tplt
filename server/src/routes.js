0da5038aa245e27213321f65d5e98d597786db23
ankuryu <pmco89@gmail.com>
2018-08-22 22:26:54 +0530

    routes changed

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,21 +1,6 @@
-/* const AuthenticationController = require('./controllers/AuthenticationController')
-const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
-const SongsController = require('./controllers/SongsController')
-const BookmarksController = require('./controllers/BookmarksController')
-const HistoriesController = require('./controllers/HistoriesController')
-
-const isAuthenticated = require('./policies/isAuthenticated')
-*/
 const invsizeController = require('./controllers/invsizeController.js');
 
 module.exports = (app) => {
-  /*
-  app.post('/register',
-    AuthenticationControllerPolicy.register,
-    AuthenticationController.register)
-  app.post('/login',
-    AuthenticationController.login)
-*/
   app.get('/api/pri/size',
     invsizeController.index);
   app.get('/api/pri/asize/:asize',
@@ -28,6 +13,54 @@ module.exports = (app) => {
     invsizeController.post);
   app.delete('/api/pri/asize/:id',
     invsizeController.remove);
+
+
+  app.get('/api/pri/asize',
+    invnameController.index);
+  app.get('/api/pri/asize/:asize',
+    invnameController.showasize);
+  app.get('/api/pri/msize/:msize',
+    invnameController.showmsize);
+  app.put('/api/pri/asize/:id',
+    invnameController.put);
+  app.post('/api/pri/asize',
+    invnameController.post);
+  app.delete('/api/pri/asize/:id',
+    invnameController.remove);
+
+
+  app.get('/api/pri/asize',
+    ldrpriController.index);
+  app.get('/api/pri/asize/:asize',
+    ldrpriController.showasize);
+  app.get('/api/pri/msize/:msize',
+    ldrpriController.showmsize);
+  app.put('/api/pri/asize/:id',
+    ldrpriController.put);
+  app.post('/api/ldrpri
+    ldrpriController.post);
+  app.delete('/api/ldrpri/:id',
+    ldrpriController.remove);
+
+};
+
+
+/* const AuthenticationController = require('./controllers/AuthenticationController')
+const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+const SongsController = require('./controllers/SongsController')
+const BookmarksController = require('./controllers/BookmarksController')
+const HistoriesController = require('./controllers/HistoriesController')
+
+const isAuthenticated = require('./policies/isAuthenticated')
+*/
+
+  /*
+  app.post('/register',
+    AuthenticationControllerPolicy.register,
+    AuthenticationController.register)
+  app.post('/login',
+    AuthenticationController.login)
+*/
 
   /*
   app.get('/bookmarks',
@@ -47,4 +80,3 @@ module.exports = (app) => {
     isAuthenticated,
     HistoriesController.post)
 	*/
-};
