63c47b8b8700e796da824d0d6d3a0c00c2570392
sunilminiasus <svgminiasus@eml.com>
2018-08-25 14:45:18 +0530

    client added  to the tplt

--- client/src/main.js
+++ client/src/main.js
@@ -1,13 +1,8 @@
 import Vue from 'vue'
-import App from './app'
-import router from './router'
+import App from './App.vue'
+
+Vue.config.productionTip = false
 
 new Vue({
-	el: '#app"',
-	router:,
-	template: '<App/>'
-	components : {App}
-})
-
-
-
+  render: h => h(App)
+}).$mount('#app')
