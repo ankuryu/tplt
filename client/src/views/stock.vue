ceee3b1fc6e40aa86ebabac19f5427a632d26a34
ankuryu <pmco89@gmail.com>
2019-03-20 14:10:22 +0530

    changes to stock view

--- client/src/views/stock.vue
+++ client/src/views/stock.vue
@@ -33,13 +33,13 @@
  
   <div class="inpcont">
  
-    <div class="name"><input type="text" v-model="stkd.mfg" placeholder="Mfg " :disabled="dsblflg == 1"></div>
-    <div class="lname"><input type="text" v-model="stkd.icode" placeholder="Icode" :disabled="dsblflg == 1"></div>
-    <div class="mbl"><input type="text" v-model="stkd.asize" placeholder="Asize" :disabled="dsblflg == 1" ></div>
-    <div class="street"><input type="text" v-model="stkd.qty" placeholder="Qty" :disabled="dsblflg == 1"></div>
-    <div class="city"><input type="text" v-model="stkd.loc" placeholder="Location" :disabled="dsblflg == 1"></div>
-    <div class="pin"><input type="text" v-model="stkd.pg" placeholder="Page" :disabled="dsblflg == 1"> </div>
-    <div class="country"><input type="text" v-model="stkd.dt" placeholder="Date" :disabled="dsblflg == 1"></div>
+    <div class="mfg"><input type="text" v-model="stkd.mfg" placeholder="Mfg " :disabled="dsblflg == 1"></div>
+    <div class="icode"><input type="text" v-model="stkd.icode" placeholder="Icode" :disabled="dsblflg == 1"></div>
+    <div class="asize"><input type="text" v-model="stkd.asize" placeholder="Asize" :disabled="dsblflg == 1" ></div>
+    <div class="qty"><input type="text" v-model="stkd.qty" placeholder="Qty" :disabled="dsblflg == 1"></div>
+    <div class="loc"><input type="text" v-model="stkd.loc" placeholder="Location" :disabled="dsblflg == 1"></div>
+    <div class="pg"><input type="text" v-model="stkd.pg" placeholder="Page" :disabled="dsblflg == 1"> </div>
+    <div class="dt"><input type="text" v-model="stkd.dt" placeholder="Date" :disabled="dsblflg == 1"></div>
     <div class="save"><button @click="savit" :disabled="dsblflg == 1">Save</button></div>
     <div class="canc"><button @click="cancit" :disabled="dsblflg == 1">Cancel</button></div>
 
@@ -73,6 +73,9 @@ export default {
   },
   data: function() {
     return {
+      sel:0,
+      dsblflg : 1,
+      opbtflg : 0,
       stkd: {
         mfg:"",
         icode:"",
@@ -95,18 +98,130 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+
+body {
+  background: #20262E;
+  padding: 20px;
+  font-family: Helvetica;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+#app {
+  background: #fff;
+  border-radius: 4px;
+  padding: 20px;
 }
+
+.hdgout {
+  text-align: center
+}
+
+.tblcont {
+  display: grid;
+  background-color: lightgray;
+  grid-gap: 5px;
+  grid-template-columns: 25px 1fr 1fr 1fr 1fr 21fr 1fr 1fr 25px;
+}
+
+.tblsr {
+  grid-column: 1 /2;
+}
+
+.tblmfg{
+  grid-column: 2/3
+}
+
+.tblicode{
+  grid-column: 3/4
+}
+
+.tblasize{
+  grid-column: 4/5
+}
+
+.tblqty{
+  grid-column: 5/6
+}
+
+.tblloc{
+  grid-column: 6/7
+}
+
+.tblpg{
+  grid-column: 7/8
+}
+
+.tbldt{
+  grid-column: 8/9
+}
+.tblsel {
+  grid-column: 9/10
+}
+
+.hdginp {
+  text-align: center
+}
+
+.inpcont {
+  display: grid;
+  background-color: lightcyan;
+  grid-gap: 3px;
+  grid-template-areas: " mfg icode asize qty" " loc pg dt"  " . save canc";
+}
+
+.mfg{
+  grid-area: mfg;
+  background-color: lightskyblue
+}
+
+.icode{
+  grid-area: icode;
+  background-color: lightgoldenrodyellow
+}
+
+.asize{
+  grid-area: asize;
+  background-color: lemonchiffon
+}
+
+.loc{
+  grid-area: loc;
+  background-color: lightblue
+}
+
+.pg{
+  grid-area: pg 
+}
+
+.dt{
+  grid-area:dt 
+}
+
+.save {
+  grid-area: save
+}
+
+.canc {
+  grid-area: canc
+}
+
+.oprcont {
+  display: grid;
+  background-color: lightgray;
+  grid-template-columns: 3em 3em 4em 4em 5em;
+  grid-template-rows: 22px;
+}
+
 li {
-  display: inline-block;
-  margin: 0 10px;
+  margin: 8px 0;
 }
-a {
-  color: #42b983;
+
+h2 {
+  font-weight: bold;
+  margin-bottom: 15px;
 }
+
+del {
+  color: rgba(0, 0, 0, 0.3);
+}
+
 </style>
