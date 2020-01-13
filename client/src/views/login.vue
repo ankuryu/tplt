90f02dbb328f826cdadaa3c89d94fb27e1cc0603
ankuryu <pmco89@gmail.com>
2020-01-13 17:23:02 +0530

    Added login and register views

---
+++ client/src/views/login.vue
@@ -0,0 +1,33 @@
+<template>
+    <v-card>
+        <v-list-item>
+            <v-title>
+                Login
+
+            </v-title>
+            <v-form>
+                <v-text-field v-model="name" label="Name">
+                <v-text-field v-model="password" label = "Password" type="password" />
+                <v-btn>submit<v-btn>
+            </v-form>
+            
+        </v-list-item>
+
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: "login",
+    props: {},
+  data: ()=>{
+      return {
+          name:"",
+          password:""
+      }
+  },
+  methods:{
+
+  }
+}
+</script>
