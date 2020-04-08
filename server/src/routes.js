ee448ec6a83f6d32e56d0d162390c1be8d31cb2c
Ankuryu <pmco89@gmail.com>
2020-04-08 19:22:10 +0530

    fixed supposed problems in route.js

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,88 +1,85 @@
-const invsizeController = require('./controllers/invsizeController.js');
-const invnameController = require('./controllers/invnameController.js');
-const ldrpriController = require('./controllers/ldrpriController.js');
-const stkController = require('./controllers/stkController.js');
-const vndrmasController = require('./controllers/vndrmasController.js');
-const cheq19Controller = require('./controllers/cheq19Controller.js');
-const outstndController = require('./controllers/outstndController.js');
+const invsizeController = require('./controllers/invsizeController.js')
+const invnameController = require('./controllers/invnameController.js')
+const ldrpriController = require('./controllers/ldrpriController.js')
+const stkController = require('./controllers/stkController.js')
+const vndrmasController = require('./controllers/vndrmasController.js')
+const cheq19Controller = require('./controllers/cheq19Controller.js')
+const outstndController = require('./controllers/outstndController.js')
 
 module.exports = (app) => {
-
-  // routes regarding asize 
+  // routes regarding asize
 
   app.get('/api/pri/asize',
-    invsizeController.index);
+    invsizeController.index)
   app.get('/api/pri/asize/:asize',
-    invsizeController.showasize);
+    invsizeController.showasize)
   app.get('/api/pri/msize/:msize',
-    invsizeController.showmsize);
+    invsizeController.showmsize)
   app.put('/api/pri/asize/:id',
-    invsizeController.put);
+    invsizeController.put)
   app.post('/api/pri/asize',
-    invsizeController.post);
+    invsizeController.post)
   app.delete('/api/pri/asize/:id',
-    invsizeController.remove);
+    invsizeController.remove)
 
   // routes regarding invname
 
   app.get('/api/pri/invname',
-    invnameController.index);
+    invnameController.index)
   app.get('/api/pri/invname/icode/:icode',
-    invnameController.showicode);
+    invnameController.showicode)
   app.get('/api/pri/invname/lcode/:lcode',
-    invnameController.showlcode);
+    invnameController.showlcode)
   app.put('/api/pri/invname/:id',
-    invnameController.put);
+    invnameController.put)
   app.post('/api/pri/invname',
-    invnameController.post);
+    invnameController.post)
   app.delete('/api/pri/invname/:id',
-    invnameController.remove);
+    invnameController.remove)
 
   // routes regarding ldrpri
 
   app.get('/api/pri/ldrpri',
-    ldrpriController.index);
+    ldrpriController.index)
   app.get('/api/pri/ldrpri/icode/:icode',
-    ldrpriController.showicode);
+    ldrpriController.showicode)
   app.get('/api/pri/ldrpri/lcode/:lcode',
-    ldrpriController.showlcode);
+    ldrpriController.showlcode)
   app.put('/api/pri/ldrpri/:id',
-    ldrpriController.put);
+    ldrpriController.put)
   app.post('/api/pri/ldrpri',
-    ldrpriController.post);
+    ldrpriController.post)
   app.delete('/api/pri/ldrpri/:id',
-    ldrpriController.remove);
+    ldrpriController.remove)
 
   // routes regarding stk
 
-  app.get('/api/stk/:ofst/:lmt', stkController.index);
-  app.get('/api/stk/mxrcs', stkController.maxRecs);
-  app.get('/api/stk/:mfg/:icode/:asize', stkController.showmcs);
-  app.get('/api/stk/:pg', stkController.showpg);
-  app.post('/api/stk', stkController.post);
-  app.put('/api/stk/:id', stkController.put);
-  app.delete('/api/stk/:id', stkController.remove);
+  app.get('/api/stk/:ofst/:lmt', stkController.index)
+  app.get('/api/stk/mxrcs', stkController.maxRecs)
+  app.get('/api/stk/:mfg/:icode/:asize', stkController.showmcs)
+  app.get('/api/stk/:pg', stkController.showpg)
+  app.post('/api/stk', stkController.post)
+  app.put('/api/stk/:id', stkController.put)
+  app.delete('/api/stk/:id', stkController.remove)
 
- // routes regarding vndrmas
+  // routes regarding vndrmas
 
-  app.get('/api/vndrmas',vndrmasController.index);
-  app.post('/api/vndrmas',vndrmasController.post);
-  app.put('/api/vndrmas/:id',vndrmasController.put);
-  app.delete('/api/vndrmas/:id',vndrmasController.remove);
+  app.get('/api/vndrmas', vndrmasController.index)
+  app.post('/api/vndrmas', vndrmasController.post)
+  app.put('/api/vndrmas/:id', vndrmasController.put)
+  app.delete('/api/vndrmas/:id', vndrmasController.remove)
 
- // routes regarding cheq19
+  // routes regarding cheq19
 
-  app.get('/api/cheq19',cheq19Controller.index);
-  app.post('/api/cheq19',cheq19Controller.post);
-  app.put('/api/cheq19/:id',cheq19Controller.put);
-  app.delete('/api/cheq19/:id',cheq19Controller.remove);
+  app.get('/api/cheq19', cheq19Controller.index)
+  app.post('/api/cheq19', cheq19Controller.post)
+  app.put('/api/cheq19/:id', cheq19Controller.put)
+  app.delete('/api/cheq19/:id', cheq19Controller.remove)
 
   // routes regardging outstanding pybls/rcvbls
 
-  app.get('/api/outs/rcv/smry/:opr', outstndController);
-
-  
-};
+  app.get('/api/outs/rcv/smry/:opr', outstndController)
+}
 
 /* const AuthenticationController = require('./controllers/AuthenticationController')
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
@@ -100,4 +97,3 @@ const isAuthenticated = require('./policies/isAuthenticated')
   app.post('/login',
     AuthenticationController.login)
 */
-
