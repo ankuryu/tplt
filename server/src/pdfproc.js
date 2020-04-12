b141136b80be4487870f4605f1e499b5a760e82d
Ankuryu <pmco89@gmail.com>
2020-04-13 01:11:52 +0530

    changes in pdfproc

--- server/src/pdfproc.js
+++ server/src/pdfproc.js
@@ -1,9 +1,25 @@
 const jsPdf = require("jsPDF");
+//  functions required for internal use and not exported
+//  Generate  Company letterhead
+//  Generate  Cheque format
+//  Generate  Bill/quo format
+//
+
+
+
 const objPdf = {
  doc = null ,
- gen_quo = function(){}, 
- gen_chq = function(){},
-  gen_vou = function() {},
+  co = "PM",
+  
+  gen_quo = function(){
+
+  }, 
+  gen_chq = function(){
+
+  },
+  gen_vou = function() {
+
+  },
 
 
 };
