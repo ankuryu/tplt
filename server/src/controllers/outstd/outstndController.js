4b379cd5576b825c6b6a05e175381ffe8d4268ba
Ankuryu <pmco89@gmail.com>
2020-06-05 00:01:43 +0530

    End of 4Jun20, ready for debug one route

--- server/src/controllers/outstd/outstndController.js
+++ server/src/controllers/outstd/outstndController.js
@@ -1,4 +1,52 @@
+const sqlio = require('.sqliteproc.js');
+const db = {};
+const dbpth = './outstd.db3'
 
 module.exports = {
+  async shwAllOutstd (req,res) {
+    var co = req.co
+    var opr = req.opr
+    var sqlstr = ""
+    if(opr="r"){
+      sqlstr = ""
+    }
+    try {
+     let rslt =  await sqlio.run_qry(dbpth,sqlstr)
+      res.send = rslt ;
+      res.status(200);
 
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to '
+      })
+    }
+  },
+  async shwPrtyOutstd (req,res) {
+    try {
+
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to '
+      })
+    }
+  },
+  async shwSmryAll(req,res) {
+    try {
+
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to '
+      })
+    }
+  },
+  async upd_outstd(req,res) {
+    try {
+
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to '
+      })
+    }
+  },
 }
+
