c776693170b934c30c5e138621846f01bbc7511e
Ankuryu <pmco89@gmail.com>
2020-06-04 10:04:39 +0530

    outstd controller skeleton added

---
+++ server/src/outstdctrlr.js
@@ -0,0 +1,12 @@
+// file outstdctrl.j  Outstanding Controller
+
+const sqlio = require('./sqliteproc.js');
+
+module.exports = {
+  async index(){},
+  async alloutsd(){},
+  async oneprty(){},
+  async allsmry(){},
+  async shwsel() {},
+  async upd_1out(){}
+}
